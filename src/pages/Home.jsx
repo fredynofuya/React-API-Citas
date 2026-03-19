05c2c80e8974d32b87bc42cae97e0ee212a967bb
fredynofuya <fredy.nofuya@udea.edu.co>
2026-03-19 17:03:50 -0500

    se agrego imagenes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,9 @@ import '../styles/HomeStyles.css';
 import cirugia from '../assets/cirugia.jpg';
 
 import consulta from "../assets/consulta.jpg";
-import uci from "../assets/uci.jpg";
+
+
+import servicio from "../assets/servicio.jpg";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useEffect } from 'react';
@@ -16,25 +18,28 @@ const Home = () => {
         document.title = 'Home';
       }, []);
   return (
-    <div >
-        <div className='Carosel'>
-          <Carousel>
+    <div>
+    <div className='Carosel'>
+          <Carousel  interval={4000} controls={false} indicators={true}> 
+            <Carousel.Item>
+              <img className="immg" src={servicio} alt={"lindo1536x864"} />
+            </Carousel.Item>
             <Carousel.Item>
               <img className="immg" src={cirugia} alt={"lindo1536x864"} />
             </Carousel.Item>
             <Carousel.Item>
               <img className="immg" src={consulta} alt={"lindo1536x864"} />
             </Carousel.Item>
-            <Carousel.Item>
-              <img className="immg" src={uci} alt={"lindo1536x864"} />
-            </Carousel.Item>
           </Carousel>
-        </div>
+    </div>
+    <div className='home-container'>
+        
         <h1>Bienvenido a TuCitaYa</h1>
         <div className='welcome-section'>
           <h2>Tu plataforma de citas médicas de confianza</h2>
           <p>Agenda tu cita de forma rápida y sencilla con los mejores profesionales de la salud</p>
         </div>    
+    </div>
     </div>
   )
 }
